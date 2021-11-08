0adaeee801649ba361996230b131f2b066b3adc1
José Carlos <josecarlosbsilva.17@gmail.com>
2021-11-08 01:32:15 -0300

    add the show book action and your tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,11 @@ class BooksController < ApplicationController
     render json: @books,
            each_serializer: Book::Index::BookSerializer, status: :ok
   end
+
+  def show
+    @book = Book.find(params[:id])
+
+    render json: @book,
+           serializer: Book::Show::BookSerializer, status: :ok
+  end
 end
