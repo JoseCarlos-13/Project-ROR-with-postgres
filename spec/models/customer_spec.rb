9f0b8f36f871cfdfb9edb7e7d99dd689d757328f
José Carlos <josecarlosbsilva.17@gmail.com>
2022-04-10 16:00:21 -0300

    add querys in the seeds file

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Customer, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:age) }
     it { is_expected.to validate_presence_of(:email) }
+
     it { is_expected.to validate_uniqueness_of(:email) }
   end
 end
