7b437e9b94002282edb2f25f65caffe3bf077654
José Carlos <josecarlosbsilva.17@gmail.com>
2022-04-17 03:59:42 -0300

    add the show action, the before_action method and the test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,23 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show]
+
   def index
     @customers = Customer.all
 
     render json: @customers,
            each_serializer: Customer::Index::CustomerSerializer,
            status: :ok
+  end
+
+  def show
+    render json: set_customer,
+           serializer: Customer::Show::CustomerSerializer,
+           status: :ok
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
