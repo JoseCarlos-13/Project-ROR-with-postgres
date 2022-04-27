54a5d00a84efc23494f52f42092e44fde3e76e91
José Carlos <josecarlosbsilva.17@gmail.com>
2022-04-27 01:49:45 -0300

    write the validations test in operator model spec file

--- spec/models/operator_spec.rb
+++ spec/models/operator_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Operator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
 end
